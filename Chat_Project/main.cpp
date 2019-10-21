8bb6abf648ae30b7bc9da2e67c6c5529c41b512f
SuperBrego <bernardo_rego@hotmail.com>
2019-10-20 21:13:36 -0300

    Escrito um pouco

--- Chat_Project/main.cpp
+++ Chat_Project/main.cpp
@@ -1,11 +1,36 @@
 #include <stdio.h>
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
 
 
+void clientSideSocket() {
+	sf::Time t1 = sf::seconds(1.f);
+	sf::TcpSocket socket;
+	sf::Socket::Status status = socket.connect("192.168.0.5", 53000, t1);
+	if (status != sf::Socket::Done)
+	{
+		printf("Erro on Client\n");
+		return;
+	}
+	
+	char data[100] = "This is a test";
+
+	// TCP socket:
+	if (socket.send(data, 100) != sf::Socket::Done)
+	{
+		printf("Erro on Sent\n");
+		return;
+	}
+}
+
 int main(){
 
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	//serverSideSocket();
+	clientSideSocket();
+
+
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 
